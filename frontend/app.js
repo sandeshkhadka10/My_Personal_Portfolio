20db1d64fb917be0020d48b9c8dda9cfff084bce
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-04-24 22:50:57 +0545

    style:changes in the display

--- frontend/app.js
+++ frontend/app.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["a passionate learner."],
+  strings : ["Build Improve."],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
