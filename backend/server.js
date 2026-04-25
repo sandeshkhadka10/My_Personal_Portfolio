9c3abdaef1a883464eec2a929e88969785ff61a6
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-04-25 23:23:46 +0545

    fix:relocate the backend files to backend folder and remove extra line of code

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-// Load environment variables from the .env file one folder above the current script.
-dotenv.config({ path: path.join(__dirname, "..", ".env") });
+// Load environment variables from backend/.env.
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
